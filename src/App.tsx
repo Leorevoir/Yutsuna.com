409b0bade28b311745a8278bf6ae621aa042be7e
Leorevoir <leo.quinzler@epitech.eu>
2025-08-09 14:40:21 +0200

    add: v.0.0.1 app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import './App.css'
+import React from 'react';
+import TerminalHeader from './components/header';
+import TerminalBody from './components/body';
+import { useTerminalState } from './hooks/state';
 
-function App() {
+/**
+ * @brief terminal app entry point
+ */
+const App: React.FC = () => {
+  const { commands, current_input, setCurrentInput, executeCommand, processor } = useTerminalState();
 
   return (
-    <>
-      <div>
-        <h1>Vite + React + TypeScript</h1>
-      </div>
-    </>
-  )
-}
+    <div className="min-h-screen bg-gray-900 text-green-400 font-mono overflow-hidden">
+      <TerminalHeader />
+      <TerminalBody
+        commands={commands}
+        current_input={current_input}
+        setCurrentInput={setCurrentInput}
+        executeCommand={executeCommand}
+        processor={processor}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
